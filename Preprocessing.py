01f89b776a7d260a3349aef42b7931e0c6c1328c
Francesco Ferrulli <francescoferrulli2000@gmail.com>
2022-05-20 16:06:02 +0200

    Integrata la conoscenza con le colonne dei conteggi delle parole positive e negative

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,3 +1,5 @@
+from bisect import bisect_left
+
 '''
    Questa funzione effettua il cleaning del dataset
    rimuovendo le colonne irrilevanti ed eliminando righe con valori mancanti
@@ -30,3 +32,52 @@ def addSentimentColumn(dataset):
     dataset.loc[dataset["rating"] == 4, "sentiment"] = 1
     dataset.loc[dataset["rating"] == 5, "sentiment"] = 1
     return dataset;
+
+def countWords(list_word, text):
+    splitted_text = text.split()
+    count = 0
+    
+    for word in splitted_text:
+        i = bisect_left(list_word, word)
+        if i != len(list_word) and list_word[i] == word:
+            count = count + 1
+    
+    return count
+
+def addNumberPositiveReview(dataset):
+    #creazione della colonna numberPositiveReview
+    if not 'numberPositiveReview' in dataset:
+        dataset.insert(4, 'numberPositiveReview', 0)
+    
+    return dataset
+    
+def addNumberNegativeReview(dataset):
+    #creazione della colonna numberNegativeReview
+    if not 'numberNegativeReview' in dataset:
+        dataset.insert(5, 'numberNegativeReview', 0)
+    
+    return dataset
+
+def addNumberPositiveSummary(dataset):
+    #creazione della colonna numberPositiveSummary
+    if not 'numberPositiveSummary' in dataset:
+        dataset.insert(6, 'numberPositiveSummary', 0)
+    
+    return dataset
+    
+def addNumberNegativeSummary(dataset):
+    #creazione della colonna numberNegativeSummary
+    if not 'numberNegativeSummary' in dataset:
+        dataset.insert(7, 'numberNegativeSummary', 0)
+    
+    return dataset
+
+def binarySearch(list_word, word):
+    for word in list_word:
+        i = bisect_left(list_word, word)
+        if i != len(list_word) and list_word[i] == word:
+            return 1
+        else:
+            return 0
+
+
